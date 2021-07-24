a71e28f99bfd28c96e79436f1f3f12e7ee266ca3
bin <tuduweb@qq.com>
2021-07-24 13:36:19 +0800

    [dp, 回溯]题解+自主N天后练习一下，还需要重新练习。

--- programming/131.分割回文串.cpp
+++ programming/131.分割回文串.cpp
@@ -21,12 +21,19 @@ class Solution {
      * 对于前2个字符aa，有两种分割方法[aa]和[a,a]。
      * 当我们每一次搜索到字符串的第i=2个字符b时，对于s[i..j]使用双指针判断是否为回文串，产生了重复的计算。
      */
+
+public:
+    vector<vector<string>> partition(string s) {
+        return solve2(s);
+    }
+
+
+#if 0
 private:
     vector<vector<int > > f;
     vector<vector<string > > ret;
     vector<string > ans;
     int n;
-public:
     void dfs(const string& s, int i) {
         //边界条件 切到底了 那么这个答案合格了
         if(i == n) {
@@ -42,7 +49,8 @@ public:
             }
         }
     }
-    vector<vector<string>> partition(string s) {
+
+    vector<vector<string>> solve1(string& s) {
         n = s.size();
         f.assign(n, vector<int >(n, true));
 
@@ -66,6 +74,73 @@ public:
         return ret;
     }
 
+#elif 1
+
+
+private:
+    int n;
+    vector<vector<string > > ans;
+    vector<string > item;
+    vector<vector<bool> > dp;
+
+    void dfs2(string& s, int start) {
+        //边界处理
+        if(start == n) {
+            ans.push_back(item);
+            //item.clear();
+            return;
+        }
+
+        for(int k = start; k < n; ++k) {
+            if(dp[start][k]) {
+                item.push_back(s.substr(start, k - start + 1));
+                dfs2(s, k + 1);
+
+                //因为"abb" "a" 能引申出两种情况 所以不能使用clear 但是可以使用pop_back
+                item.pop_back();
+            }
+        }
+        // //该项不可分
+        // if(dp[start][n - 1] == false) {
+        //     item.clear();
+        // }
+
+    }
+
+    /**
+     * Accepted
+     * 32/32 cases passed (124 ms)
+     * Your runtime beats 83.89 % of cpp submissions
+     * Your memory usage beats 80.69 % of cpp submissions (73.8 MB)
+     */
+    vector<vector<string>> solve2(string& s) {
+        n = s.length();
+
+        //start, end
+        dp.assign(n, vector<bool>(n, true));
+
+        for(int i = 1; i < n; ++i) {
+            for(int k = i - 1; k >= 0; --k) {
+                //转移条件
+                dp[k][i] = dp[k + 1][i - 1] && (s[k] == s[i]);
+            }
+        }
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j) {
+                cout << dp[i][j] << " ";
+            }
+            cout << endl;
+        }
+        
+        //从0开始dfs
+        dfs2(s, 0);
+
+        return ans;
+    }
+
+
+#endif
 
 
 };
