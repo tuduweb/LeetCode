2599c3f75ab6b2821410d3bb2e78f6be4dc5596d
bin <tuduweb@qq.com>
2021-07-28 21:49:42 +0800

    [二分查找]学习官方题解，把搜边界的情况放到二分查找中。

--- programming/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ programming/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -18,7 +18,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        return solve1(nums, target);
+        return solve2(nums, target);
     }
     /**
      * 自主 二分查找+while?
@@ -60,6 +60,51 @@ public:
         
         return {l, r};
 
+    }
+
+
+    /**
+     * Accepted
+     * 88/88 cases passed (8 ms)
+     * Your runtime beats 78.88 % of cpp submissions
+     * Your memory usage beats 55.04 % of cpp submissions (13.3 MB)
+     */
+    vector<int> solve2(vector<int>& nums, int target) {
+        if(nums.size() == 0) return {-1, -1};
+        int n = nums.size();
+
+        int l = 0, r = n - 1, m = (l + r) / 2;
+
+        int leftAns = -1;
+
+        while(l <= r) {
+            m = (l + r) / 2;
+            if(nums[m] >= target) {
+                r = m - 1;
+                leftAns = m;
+            }else{
+                l = m + 1;
+            }
+        }
+
+        int rightAns = n;
+        l = 0, r = n - 1;
+        while(l <= r) {
+            m = (l + r) / 2;
+            if(nums[m] > target) {
+                r = m - 1;
+            }else{
+                l = m + 1;
+                rightAns = m;
+            }
+        }
+
+        cout << "l " << leftAns << "  r " << rightAns;
+
+        if(leftAns <= rightAns && leftAns >= 0 && rightAns < n && nums[leftAns] == target && nums[rightAns] == target)
+            return {leftAns, rightAns};
+        
+        return {-1, -1};
     }
 };
 // @lc code=end
